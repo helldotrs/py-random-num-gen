8341ac1603b94e0d92cc4798c0bbe06008806034
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-02-05 12:05:14 +0100

    Create random-01.py

---
+++ random-01.py
@@ -0,0 +1,3 @@
+#the idea is exploring random number generation without using a pre existing random library, or looking into random num gen in advance. Why? I have no idea.
+
+#thinking time might be used as a seed. explore more later.
