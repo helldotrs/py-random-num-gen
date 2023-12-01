df416c4a67c42cdf180d317ad07e66052c847447
Rebekah <macrosystem@proton.me>
2023-12-01 14:15:09 +0100

    Update test-microsecond.py

--- test-microsecond.py
+++ test-microsecond.py
@@ -4,8 +4,8 @@ for a in range(10):
     current_time = datetime.now()
     microseconds_rightmost_digit = current_time.microsecond % 10
 
-    print("run " + str(a) +" results:")
-    print("current_time:" + current_time)
-    print("current_time.microsecond:" + current_time.microsecond)
-    print("microseconds_rightmost_digit:" + microseconds_rightmost_digit)
+    print("run " + str(a)                    +" results:")
+    print("current_time:"                    + str(current_time))
+    print("current_time.microsecond:"        + str(current_time.microsecond))
+    print("microseconds_rightmost_digit:"    + str(microseconds_rightmost_digit))
     
