4687eb46937640502e2676b71cf050ad653bf37b
Rebekah <macrosystem@proton.me>
2023-12-01 14:20:56 +0100

    Update xor-loop.py

--- xor-loop.py
+++ xor-loop.py
@@ -1,1 +1,16 @@
+import datetime
 
+strength = 16
+def gen_rand_binary():
+  binary = 0
+  for a in range(strength):
+    while True:
+      current_time = datetime.datetime.now()
+      binary = 1 - binary #xor
+
+      #FIXME:
+      current_time = datetime.datetime.now()
+      microseconds_rightmost_digit = current_time.microsecond % 10
+      if microseconds_rightmost_digit == 0:
+          print("Breaking the loop.")
+          break # and output binary?
