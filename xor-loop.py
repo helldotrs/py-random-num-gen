395f32c40f46230d167ce4e4298d8e63f5e30038
Rebekah <macrosystem@proton.me>
2023-12-02 16:40:06 +0100

    Update xor-loop.py

--- xor-loop.py
+++ xor-loop.py
@@ -1,15 +1,21 @@
 import datetime
 
+test = True
 strength = 16
 def gen_rand_bool():
     rand_value = 0
     output     = 0
     
     for a in range(strength):
+        if test:
+            print("---range loop itr.---")
         while True: #xor loop
             time_value = datetime.datetime.now().microsecond % 10
             rand_value = 1 - rand_value
-          
+
+            if test:
+                print("xor loop itr. var: output: " + str(output) + ". time_value: " + str(time value) + ". rand_value: " + str(rand_value) + ". ")
+
             if not time_value: 
                 output = output != rand_value
                 break
